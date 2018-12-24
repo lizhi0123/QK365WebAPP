ec2927b9835374f7629bf9aa466975b955fc65b1
Sunny <zhang_zhi_li@163.com>
2018-12-24 12:47:01 +0800

    首页页面显示

--- qk365wap/src/pages/home/Home.vue
+++ qk365wap/src/pages/home/Home.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="app">
-    <router-view/>
+    Home Test
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
 
 <style>
+
 </style>
--- qk365wap/src/router/index.js
+++ qk365wap/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home/Home'
+
 
 Vue.use(Router)
 
@@ -10,6 +12,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    }, {
+      path: '/home',
+      name: 'Home',
+      component: Home
     }
   ]
 })
