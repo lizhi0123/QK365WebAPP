15c07b2aec639448a46b41f3b8a1dca375cda24f
Sunny <zhang_zhi_li@163.com>
2018-12-28 17:58:43 +0800

    设置首页 head nav

--- qk365wap/src/pages/home/Home.vue
+++ qk365wap/src/pages/home/Home.vue
@@ -1,20 +1,48 @@
 <template>
-  <div id="app">
-    Home Test
-    <br>
-    <br>
-    <router-link to= "/helloworld">
-        Helloworld 
-    </router-link>
+  <div id="app" class="app">
+     <home-head-nav> </home-head-nav>
+     <div class="container">
+         <p><br><br><br>Home Test <br><br><br><br><br><br> </p>
+         <p><br><br><br>Home Test <br><br><br><br><br><br> </p>
+         <p><br><br><br>Home Test <br><br><br><br><br><br> </p>
+         <p><br><br><br>Home Test <br><br><br><br><br><br> </p>
+         <p><br><br><br>Home Test <br><br><br><br><br><br> </p>
+         <p><br><br><br>Home Test <br><br><br><br><br><br> </p>
+         <p><br><br><br>Home Test <br><br><br><br><br><br> </p>
+         <p><br><br><br>Home Test <br><br><br><br><br><br> </p>
+         <p><br><br><br>Home Test <br><br><br><br><br><br> </p>
+         <p><br><br><br>Home Test <br><br><br><br><br><br> </p>
+         <p><br><br><br>Home Test <br><br><br><br><br><br> </p>
+         <p><br><br><br>Home Test <br><br><br><br><br><br> </p>
+         <p><br><br><br>Home Test <br><br><br><br><br><br> </p>
+         <p><br><br><br>Home Test <br><br><br><br><br><br> </p>
+         <p><br><br><br>Home Test <br><br><br><br><br><br> </p>
+         <p><br><br><br>Home Test <br><br><br><br><br><br> </p>
+         <p><br><br><br>Home Test <br><br><br><br><br><br> </p>
+        Home Test
+        <br>
+        <br>
+        <router-link to= "/helloworld">
+            Helloworld 
+        </router-link>
+     </div>
   </div>
 </template>
 
 <script>
+    import HomeHeadNav from './components/HomeHeadNav.vue'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeadNav
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
+.app
+    width:100%
+    overflow: hidden
+    background-color: yellow
 
 </style>
---
+++ qk365wap/src/pages/home/components/HomeHeadNav.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            返回
+        </div>
+    </div>
+    
+</template>
+
+<script >
+export default {
+  name: 'HomeHeadNav'
+}
+
+    
+</script>
+
+<style lang="stylus"  scoped>
+.header
+    display: flex
+    background: #FC7F08FF
+    margin-top: .0rem
+    margin-left: .0rem
+    height: .86rem
+    width: 100%
+    position: relative 
+    .header-left
+        // float: left
+        margin-top: .2rem
+        // padding-left: .64rem
+        margin-left: .2rem
+        width: .84rem
+        height: .58rem
+        line-height: .58rem
+        text-align: center
+        vertical-align: middle
+        background: #21FE06FF
+</style>
