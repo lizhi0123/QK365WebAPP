8bff1731d1ea5d949344be95e84f4ee165176bda
Sunny <zhang_zhi_li@163.com>
2019-01-03 13:41:30 +0800

    首页导航不变的修改

--- qk365wap/src/pages/home/Home.vue
+++ qk365wap/src/pages/home/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="app" class="app">
+  <div id="app" class="app" ref="app">
      <!-- <home-head-nav> </home-head-nav> -->
      
-     <div class="container-warp">
+     <div class="container-warp" ref="containerWarp">
         <div class="header">
             <div class="header-left">
                 返回
@@ -36,15 +36,51 @@ export default {
   name: 'Home',
   components: {
     HomeHeadNav
+  },
+  data (){
+    return {
+        clientWidth:'',
+        clientHeight:''
+    }
+  },
+  mounted (){
+    // this.clientHeight = '${document.documentElement.clientHeight}'
+    this.clientHeight = document.documentElement.clientHeight;
+    this.clientWidth = document.documentElement.clientWidth;
+    window.onresize = function temp() {
+        this.clientHeight = document.documentElement.clientHeight;
+        this.clientWidth =  document.documentElement.clientWidth;
+      };
+  },
+   watch: {
+      // 如果 `clientHeight` 发生改变，这个函数就会运行
+      clientHeight: function () {
+        this.changeClientHeightFixed(this.clientHeight);
+      },
+      clientWidth: function () {
+        this.changeClientWidthFixed(this.clientWidth);
+      }
+    },
+    methods:{
+      changeClientHeightFixed(clientHeight){ 
+      //动态修改样式
+        this.$refs.app.style.height = clientHeight+'px';
+        // this.$refs.containerWarp.style.height = clientHeight+'px';
+
+      },
+      changeClientWidthFixed(clientWidth){ 
+        this.$refs.app.style.width = clientWidth+'px';
+        // this.$refs.containerWarp.style.height = clientHeight+'px';
+      },
   }
 }
 </script>
 
 <style lang="stylus" scoped>
 .container-warp
-    width: 7rem
-    // height: 100%
-    height: 14rem
+    width: 100%
+    height: 100%
+    // height: 14rem
     overflow: hidden
     position: relative
     background-color: red
