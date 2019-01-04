76529b06a3e855ba4d5e979393ec8f32d0274f9a
Sunny <zhang_zhi_li@163.com>
2019-01-04 14:21:29 +0800

    首页导航

--- qk365wap/src/pages/home/components/HomeHeadNav.vue
+++ qk365wap/src/pages/home/components/HomeHeadNav.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="header">
-        <div class="header-left iconfont">
-            &#xe679; 返回 
+        <div class="header-left">
+            <div class="header-left-city">
+                上海 
+            </div>
+             <div class="iconfont header-left-img">
+                 &#xe749;
+             </div>
         </div>
-        <div class="homeNav-img-div">
-            <img style="width: 100%; height:100%; top: 0; left: 0;" src="@/assets/images/home_qkLogo.png">
-        </div>
+            <div class="homeNav-img-div">
+                <!-- 标题 -->
+                <img style="width: 100%; height:100%; top: 0; left: 0;" src="@/assets/images/home_qkLogo.png">
+            </div>
     </div>
     
 </template>
@@ -20,33 +26,51 @@ export default {
 
 <style lang="stylus"  scoped>
 @import '~@/assets/styles/varibles.styl'
-.header
-    position: fixed
-    left:0
-    top:0
-    height:$navHeight
-    line-height:$navHeight
-    width: 100%
-    z-index: 9999
-    background: #33CC00FF
+$headerCityWidth = 0.8rem
 
-    .header-left
+.header 
+    position: fixed;
+    width: 100%;
+    height:$navHeight;
+    line-height:$navHeight;
+    line-height: $navHeight;
+    z-index: 9999;
+    border-bottom: .01rem solid #e5e5e5
+
+    .header-left 
+        position absolute
         font-size: .28rem
-        // float: left
-        margin-top: .2rem
-        // padding-left: .64rem
-        margin-left: .2rem
-        width: 1.4rem
+        margin: auto 
+        top: 0;
+        bottom: 0;
+        left: .2rem
+        width: ($headerCityWidth + 0.3rem);
         height: .58rem
         line-height: .58rem
-        text-align: center
-        vertical-align: middle
-        background: #21FE06FF
-    .homeNav-img-div
-        width .7rem
-        height .25rem
-        // text-align center
-        background-color red
-        margin: 0 auto
+         // text-align: center
+        // vertical-align: middle
+        // color $greenTitleTextColor
+        // background-color red
+    
+        .header-left-city
+            position: absolute;
+            top:0; left: 0; bottom: 0;
+            width: $headerCityWidth;
+            text-align: center;
+            color: $greenTitleTextColor
+        .header-left-img
+            position absolute
+            top:0; left: $headerCityWidth; bottom:0;
+            width: 0.3rem;
+    .homeNav-img-div 
+        position: absolute;
+        width: 1.4rem;
+        height: .5rem;
+        line-height: .25rem;
+        text-align: center;
+        margin: auto; 
+        top: 0; left: 0; bottom: 0; right: 0;
+
+         
                   
 </style>
