2a6bd50f312297d66132e63bd03f33659b91e800
Sunny <zhang_zhi_li@163.com>
2019-01-04 18:03:32 +0800

    home banner

--- qk365wap/src/pages/home/components/HomeSwiper.vue
+++ qk365wap/src/pages/home/components/HomeSwiper.vue
@@ -1,50 +1,24 @@
 <template>
-  <div class="wrapper">
-    <swiper :options="swiperOption">
+  <!-- <div class="wrapper"> -->
+    <swiper :options="swiperOption" class="wrapper">
       <swiper-slide >
         <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1812/e0/7aed5dde53dabe02.jpg_750x200_9b7a65fd.jpg" />
     </swiper-slide>
     <swiper-slide >
         <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1811/7c/8e5c4ab8ee8b7402.jpg_750x200_dd7a38dd.jpg" />
     </swiper-slide>
-
           <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
-  </div>
+  <!-- </div> -->
 </template>
 
 <script >
 export default {
-  name: 'HomeSwiper',
-  props: {
-    list: Array
-  },
-  data () {
-    return {
-      swiperOption: {
-        pagination: '.swiper-pagination',
-        loop: true
-      },
-      swiperList: [{
-        id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1812/e0/7aed5dde53dabe02.jpg_750x200_9b7a65fd.jpg'
-      }, {
-        id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/7c/8e5c4ab8ee8b7402.jpg_750x200_dd7a38dd.jpg'
-      }]
-    }
-  },
-  computed: {
-    showSwiper () {
-      return this.list.length
-    }
-  }
+  name: 'HomeSwiper'
 }
 </script>
 
 <style lang="stylus" scoped="">
-.wrapper >>> .swiper-pagination-bullet-active
-        background: #fff
 .wrapper
     overflow: hidden
     width: 100%
